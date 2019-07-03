c50176b5994c9bbee288683f2299d63a601914e5
88128 <88128@farnborough.ac.uk>
2019-07-03 14:25:27 +0100

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,31 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+
 public class Main {
 
     public static Connection db = null;
 
     public static void main(String[] args) {
-
+    openDatabase("CourseWork.db");
+    // code to get data from here
+    closeDatabase();
     }
 
-    public static void openDatabase(String dbOpen){
+    private static void openDatabase(String dbFile){
         try{
             Class.forName("org.sqlite.JDBC");
             SQLiteConfig config = new SQLiteConfig();
             config.enforceForeignKeys(true);
-            db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbOpen, config.toProperties());
+            db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
             System.out.println("\nDatabase connection established successfully.");
-
         } catch (Exception exception) {
             System.out.println("\nConnection Error:\n" + exception.getMessage());
         }
     }
 
 
-    public static void closeDatabase(){
+    private static void closeDatabase(){
         try{
             db.close();
             System.out.println("\nDatabase connection established successfully.");
---
+++ src/main/java/Scoreboard.java
@@ -0,0 +1,14 @@
+public static void DeleteID(int id) {
+
+        try {
+
+        PreparedStatement ps = Main.db.prepareStatement("DELETE FROM -- WHERE Id = 1");
+
+        ps.setInt(1, id);
+
+        ps.execute();
+
+        } catch (Exception exception) {
+        System.out.println("Database error: " + exception.getMessage());
+        }
+        }
