255187d3f940de26b45e58413e6bf40be30cc328
88128 <88128@farnborough.ac.uk>
2019-10-07 15:59:55 +0100

    Phase 2 L2.1

---
+++ src/main/java/Controllers/ListQuizzes.java
@@ -0,0 +1,54 @@
+package Controllers;
+import Server.Main;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class ListQuizzes {
+
+    public static void ListQuizzes() {
+        try {
+
+            PreparedStatement ps = Main.db.prepareStatement("SELECT QuizTitle, QuizSkillLevel, QuizId FROM Quizzes");
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                String QuizTitle = results.getString(1);
+                int QuizSkillLevel = results.getInt(2);
+                int QuizID = results.getInt(3);
+            }
+
+        } catch (Exception exception) {
+            System.out.println("Database Error:" + exception.getMessage());
+        }
+    }
+
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listQuizzes() {
+        System.out.println("Coursework/list");
+        JSONArray list = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT QuizTitle, QuizSkillLevel, QuizId FROM Quizzes");
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                JSONObject item = new JSONObject();
+                String QuizTitle = results.getString(1);
+                int QuizSkillLevel = results.getInt(2);
+                int QuizID = results.getInt(3);
+            }
+            return list.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+        }
+    }
+}
+
