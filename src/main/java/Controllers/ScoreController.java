b864356b53d40c2a732b6197fe7db11cbf2416b7
88128 <88128@farnborough.ac.uk>
2019-10-18 10:59:51 +0100

    Phase 2 L4.1

--- src/main/java/Controllers/ScoreController.java
+++ src/main/java/Controllers/ScoreController.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
@@ -29,13 +30,39 @@ public class ScoreController {
     }
 
     @GET
+    @Path("get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getThing(@PathParam("UserName") String Username){
+        if (id == null) {
+            throw new Exception("Thing's 'id' is missing in the HTTP request's URL.");
+        }
+        System.out.println("thing/get/" + UserName);
+        JSONObject item = new JSONObject();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserName, Scores FROM Things WHERE UserName");
+            ps.setInt(1, id);
+            ResultSet results = ps.executeQuery();
+            if (results.next()) {
+                JSONObject item = new JSONObject();
+                item.put("UserName",UserName);
+                item.put("TotalScore", results.getInt(2));
+                get.add(item);
+            }
+            return item.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @GET
     @Path("list")
     @Produces(MediaType.APPLICATION_JSON)
     public String listScores() {
         System.out.println("Coursework/list");
         JSONArray list = new JSONArray();
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT Username,TotalScore FROM Scores");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT Username, TotalScore FROM Scores");
             ResultSet results = ps.executeQuery();
             while (results.next()) {
                 JSONObject item = new JSONObject();
